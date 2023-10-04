4c2af82767d720e7ebf07f4a4ee7942d0b38783f
Kevin Zehner <kevzehner@gmail.com>
2023-10-04 11:54:00 +0100

    initial commit, javascript to be added

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//create variables for input fields
+
+const bill = document.getElementById("bill");
+const people = document.getElementById("numberOfPeople");
+
+const customButton = document.querySelector('.tip-button[value="custom"]');
+const customInput = document.getElementById("customTipInput");
+
+//variables for results
+
+const tipPerPerson = document.getElementById("tipPerPerson");
+const total = document.getElementById("total");
