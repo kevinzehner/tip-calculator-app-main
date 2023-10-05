71e27c783da76d024ac54dbe4ca924bf61cdd935
Kevin Zehner <kevzehner@gmail.com>
2023-10-05 19:02:19 +0100

    fixed number of people input field bug

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ const total = document.getElementById("total");
 
 // Function to calculate and update the tip and total
 function calculateTip() {
-  // Get the bill amount and number of people
-  const billAmount = parseFloat(bill.value) || 0; // Use 0 if parsing fails
-  const numberOfPeople = parseFloat(people.value) || 1; // Use 1 if parsing fails
+  // Get the bill amount, number of people, and custom tip percentage
+  const billAmount = parseFloat(bill.value) || 0;
+  const numberOfPeople = parseFloat(people.value) || 1;
+  const customTipValue = parseFloat(customTipInput.value) || 0;
 
   // Get the selected tip percentage
   let tipPercentage = 0;
-
   const selectedButton = document.querySelector(".tip-button.active");
   if (selectedButton) {
     tipPercentage = parseFloat(selectedButton.value);
   }
 
   // Calculate the tip and total per person
-  const tipAmount = (billAmount * tipPercentage) / 100;
+  const tipAmount = (billAmount * (tipPercentage || customTipValue)) / 100;
   const totalPerPerson = (billAmount + tipAmount) / numberOfPeople;
 
   // Display the calculated values
@@ -47,36 +47,10 @@ tipButtons.forEach((button) => {
   });
 });
 
-// Event listener for custom tip input field
-customTipInput.addEventListener("input", () => {
-  // Get the custom tip percentage value from the input field
-  const customTipValue = parseFloat(customTipInput.value) || 0; // Use 0 if parsing fails
+// Event listeners for input fields
+bill.addEventListener("input", calculateTip);
+people.addEventListener("input", calculateTip);
+customTipInput.addEventListener("input", calculateTip);
 
-  // Get the bill amount and number of people
-  const billAmount = parseFloat(bill.value) || 0;
-  const numberOfPeople = parseFloat(people.value) || 1;
-
-  // Calculate the tip and total per person
-  const tipAmount = (billAmount * customTipValue) / 100;
-  const totalPerPerson = (billAmount + tipAmount) / numberOfPeople;
-
-  // Display the calculated values
-  tipPerPerson.textContent = `$${tipAmount.toFixed(2)}`;
-  total.textContent = `$${totalPerPerson.toFixed(2)}`;
-});
-
-// Function to reset all input fields and results
-function resetCalculator() {
-  bill.value = "";
-  people.value = "";
-  customTipInput.value = "";
-  tipPerPerson.textContent = "$0.00";
-  total.textContent = "$0.00";
-  tipButtons.forEach((button) => button.classList.remove("active"));
-}
-
-// Add an event listener to the reset button
-const resetButton = document.querySelector(".reset-button");
-resetButton.addEventListener("click", resetCalculator);
 // Initial calculation when the page loads
 calculateTip();
